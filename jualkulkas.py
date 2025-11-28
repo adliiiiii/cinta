7d6970e1334d0a493ead5b6170ee37b2f3cf0249
adliiiiii <rifkyaladli61@gmail.com>
2025-11-28 20:13:46 +0700

    cinta

--- jualkulkas.py
+++ jualkulkas.py
@@ -2,11 +2,84 @@ import time
 from datetime import date
 from colorama import Fore, Style, Back
 
+data = []     # DATA USER DISIMPAN DI LIST
+keranjang = []
+current_user = ""
+
+# ==============================================
+# FUNGSI SIMPAN & LOAD USER
+# ==============================================
+def simpan_user_ke_file():
+    with open("user.txt", "w", encoding="utf-8") as f:
+        for user, pw in data:
+            f.write(f"{user},{pw}\n")
+
+def load_user_dari_file():
+    try:
+        with open("user.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                user, pw = line.strip().split(",")
+                data.append([user, pw])
+    except FileNotFoundError:
+        pass
+
+# PANGGIL LOAD
+load_user_dari_file()
+
+# ==============================================
+# LOGIN & REGISTER
+# ==============================================
+def masukan_user():
+    global current_user
+    user = input("Masukan User : ")
+    pw = input("Masukan Password : ")
+
+    if [user, pw] in data:
+        current_user = user
+        print("Login Berhasil!\n")
+        return True
+    else:
+        print("Akun tidak ditemukan!\n")
+        return False
+
+def buat_akun():
+    user = input("Masukan User : ")
+    pw = input("Masukan Password : ")
+    konfirmasi = input("Konfirmasi Password : ")
+
+    while konfirmasi != pw:
+        print("Password tidak sama, coba lagi!")
+        konfirmasi = input("Konfirmasi Password : ")
+
+    data.append([user, pw])
+    simpan_user_ke_file()
+    print("Akun berhasil dibuat!\n")
+
+# ==============================================
+# CEK USER
+# ==============================================
+while True:
+    akun = input("Sudah Punya Akun [Y/N] : ").lower()
+
+    if akun == "y":
+        if masukan_user():
+            break
+
+    elif akun == "n":
+        buat_akun()
+        print("Silakan login kembali.\n")
+
+    else:
+        print("Pilihan tidak valid!\n")
+
+# ==============================================
+# DATA PRODUK
+# ==============================================
 produk = {
     1: "Mesin Cuci",
-    2: "Kulkas    ",
-    3: "AC        ",
-    4: "Dispenser ",
+    2: "Kulkas",
+    3: "AC",
+    4: "Dispenser",
     5: "Television",
 }
 
@@ -24,49 +97,9 @@ kurir = {
     3: "Kurir Reguler",
 }
 
-keranjang = []
-data = []   
-
-def masukan_user():
-    user = input("Masukan User : ")
-    pw = input("Masukan Password : ")
-
-    if [user, pw] in data:
-        print("Login Berhasil!\n")
-        return True
-    else:
-        print("Akun tidak ditemukan!\n")
-        return False
-        
-
-def buat_akun():
-    user = input("Masukan User : ")
-    pw = input("Masukan Password : ")
-    konfirmasi = input("Konfirmasi Password : ")
-
-    while konfirmasi != pw:
-        print("Password tidak sama, coba lagi!")
-        konfirmasi = input("Konfirmasi Password : ")
-
-    data.append([user, pw])
-    print("Akun berhasil dibuat!\n")
-
-while True:
-    akun = input("Sudah Punya Akun [Y/N] : ").lower()
-
-    if akun == "y":
-        if masukan_user():   
-            break
-
-    elif akun == "n":
-        buat_akun()
-        print("Silakan login kembali.\n")  
-
-    else:
-        print("Pilihan tidak valid!\n")
-
-
-
+# ==============================================
+# TAMPIL PRODUK
+# ==============================================
 print("="*70)
 print("                          ELEKTRONIK KITA                             ")
 print("="*70)
@@ -77,9 +110,9 @@ for i in produk:
     print(f"{i:<3}| {produk[i]:18} | Rp {harga[i]:,}")
 print("-"*70)
 
-# ============================
+# ==============================================
 # INPUT PRODUK
-# ============================
+# ==============================================
 while True:
     pilih_id = int(input("Pilih Id produk: "))
     if pilih_id not in produk:
@@ -98,16 +131,18 @@ while True:
     if lagi.lower() != "y":
         break
 
-print("-"*70)
+# ==============================================
+# INPUT DATA PENERIMA
+# ==============================================
 nama = input("Nama Penerima : ")
 alamat = input("Alamat Penerima : ")
 tlp = input("No Hp: ")
 
 total_barang = 0
 
-# ============================
+# ==============================================
 # TAMPILKAN DETAIL PRODUK
-# ============================
+# ==============================================
 print("="*70)
 print("No | Nama Barang        | Harga Satuan   | Jumlah | Total")
 print("-"*70)
@@ -122,9 +157,9 @@ for item in keranjang:
 
 print("-"*70)
 
-# ============================
+# ==============================================
 # DISKON
-# ============================
+# ==============================================
 if total_barang >= 20000000:
     diskon = int(total_barang * 0.10)
     persen = "10%"
@@ -135,13 +170,10 @@ else:
     diskon = 0
     persen = "0%"
 
-# ============================
+# ==============================================
 # METODE PENGIRIMAN
-# ============================
-print("\n============================================================")
-print("                      Metode Pengiriman                       ")
-print("="*60)
-
+# ==============================================
+print("\n================== METODE PENGIRIMAN ==================")
 for i in kurir:
     print(f"{i:<3}| {kurir[i]}")
 print("-"*60)
@@ -168,7 +200,6 @@ else:
 total_bayar = total_barang - diskon
 total_akhir = total_bayar + ongkir
 
-
 print("-"*60)
 print(f"Jumlah Bayar  : Rp {total_barang:,.0f}")
 print(f"Diskon {persen:<4}   : Rp {diskon:,.0f}")
@@ -177,13 +208,14 @@ print(f"Ongkir        : Rp {ongkir:,.0f}")
 print(f"Total Akhir   : Rp {total_akhir:,.0f}")
 print("-"*60)
 
-# ============================
-# LOADING
-# ============================
+# ==============================================
+# PEMBAYARAN
+# ==============================================
 tgl = date.today()
+
 for i in range(1, 11):
     print(f"Loading: {i*10}%", end="\r")
-    time.sleep(0.2)
+    time.sleep(0.1)
 
 konfir = input("\nApakah anda yakin ingin melakukan pembayaran? (Y/N): ")
 
@@ -191,32 +223,21 @@ if konfir.lower() == "y":
     while True:
         bayar = int(input("Masukan Jumlah Bayar : "))
 
-        if bayar > total_akhir:
+        if bayar >= total_akhir:
             kembalian = bayar - total_akhir
             print("\nPembayaran Berhasil!")
-            print(f"Anda Membayar Sebesar Rp.{bayar:,.0f}")
-            print(f"Kembalian Anda : Rp.{kembalian:,.0f}")
             break
-
-        elif bayar == total_akhir:
-            print("\nPembayaran Berhasil!")
-            print(f"Anda Membayar Sebesar Rp.{bayar:,.0f}")
-            print("Uang Anda Pas")
-            break
-
         else:
-            print("Uang Anda Kurang! Silakan masukan ulang.")
+            print("Uang Anda Kurang!")
 else:
     print("\nPembayaran dibatalkan")
+    exit()
 
-# ============================
-# STRUK
-# ============================
 print(Back.WHITE + "")
 print(Fore.YELLOW + "\n===================== ELEKTRONIK KITA ======================")
 print(tgl)
-print(f"Nama Pengguna : {akun[0]}")
-print("\nNama Penerima :", nama)
+print(f"Nama Pengguna : {current_user}")
+print("Nama Penerima :", nama)
 print("Alamat        :", alamat)
 print("No HP         :", tlp)
 print("Pengantaran   :", metode_kirim)
@@ -238,13 +259,13 @@ print(f"Kembalian    : Rp.{kembalian:,.0f}")
 print("\n                       TERIMA KASIH")
 print(Style.RESET_ALL)
 
-# ============================
+# ==============================================
 # CETAK STRUK KE FILE
-# ============================
+# ==============================================
 with open("riwayat.txt", "w", encoding="utf-8") as s:
     s.write("===================== ELEKTRONIK KITA ======================\n")
-    s.write(f"{tgl}\n\n")
-    s.write(f"Nama Pengguna : {akun[0]}")
+    s.write(f"{tgl}\n")
+    s.write(f"Nama Pengguna : {current_user}\n")
     s.write(f"Nama Penerima : {nama}\n")
     s.write(f"Alamat        : {alamat}\n")
     s.write(f"No HP         : {tlp}\n")
@@ -267,6 +288,4 @@ with open("riwayat.txt", "w", encoding="utf-8") as s:
     s.write(f"Kembalian    : Rp {kembalian:,}\n")
     s.write("\n                     TERIMA KASIH\n")
 
-
-
-
+print("\nStruk berhasil dicetak ke file: riwayat.txt")
