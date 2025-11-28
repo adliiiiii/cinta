cce431e4e866ea5252d07cf43006de33e7d9875b
adliiiiii <rifkyaladli61@gmail.com>
2025-11-28 20:18:44 +0700

    a

--- jualkulkas.py
+++ jualkulkas.py
@@ -2,13 +2,13 @@ import time
 from datetime import date
 from colorama import Fore, Style, Back
 
-data = []     # DATA USER DISIMPAN DI LIST
-keranjang = []
+data = []        # Data user
 current_user = ""
+riwayat_file = "riwayat.txt"
 
-# ==============================================
-# FUNGSI SIMPAN & LOAD USER
-# ==============================================
+# ============================================
+# SIMPAN & LOAD USER
+# ============================================
 def simpan_user_ke_file():
     with open("user.txt", "w", encoding="utf-8") as f:
         for user, pw in data:
@@ -23,27 +23,31 @@ def load_user_dari_file():
     except FileNotFoundError:
         pass
 
-# PANGGIL LOAD
+# Panggil load
 load_user_dari_file()
 
-# ==============================================
+# ============================================
 # LOGIN & REGISTER
-# ==============================================
-def masukan_user():
+# ============================================
+def login():
     global current_user
-    user = input("Masukan User : ")
-    pw = input("Masukan Password : ")
 
-    if [user, pw] in data:
-        current_user = user
-        print("Login Berhasil!\n")
-        return True
-    else:
-        print("Akun tidak ditemukan!\n")
-        return False
+    while True:
+        print("\n=== LOGIN ===")
+        user = input("Masukan User : ")
+        pw = input("Masukan Password : ")
 
-def buat_akun():
-    user = input("Masukan User : ")
+        if [user, pw] in data:
+            current_user = user
+            print("Login Berhasil!\n")
+            return True
+        else:
+            print("Akun tidak ditemukan!\n")
+            return False
+
+def register():
+    print("\n=== REGISTRASI ===")
+    user = input("Masukan Username : ")
     pw = input("Masukan Password : ")
     konfirmasi = input("Konfirmasi Password : ")
 
@@ -55,26 +59,9 @@ def buat_akun():
     simpan_user_ke_file()
     print("Akun berhasil dibuat!\n")
 
-# ==============================================
-# CEK USER
-# ==============================================
-while True:
-    akun = input("Sudah Punya Akun [Y/N] : ").lower()
-
-    if akun == "y":
-        if masukan_user():
-            break
-
-    elif akun == "n":
-        buat_akun()
-        print("Silakan login kembali.\n")
-
-    else:
-        print("Pilihan tidak valid!\n")
-
-# ==============================================
+# ============================================
 # DATA PRODUK
-# ==============================================
+# ============================================
 produk = {
     1: "Mesin Cuci",
     2: "Kulkas",
@@ -97,195 +84,187 @@ kurir = {
     3: "Kurir Reguler",
 }
 
-# ==============================================
-# TAMPIL PRODUK
-# ==============================================
-print("="*70)
-print("                          ELEKTRONIK KITA                             ")
-print("="*70)
+# ============================================
+# MENAMPILKAN PRODUK
+# ============================================
+def tampilkan_produk():
+    print("="*60)
+    print("                     ELEKTRONIK KITA")
+    print("="*60)
+    print("Id | Nama Produk        | Harga")
+    print("-"*60)
+    for i in produk:
+        print(f"{i:<3}| {produk[i]:18} | Rp {harga[i]:,}")
+    print("-"*60)
 
-print("Id | Nama Produk        | Harga")
-print("-"*70)
-for i in produk:
-    print(f"{i:<3}| {produk[i]:18} | Rp {harga[i]:,}")
-print("-"*70)
+# ============================================
+# FUNGSI BELANJA
+# ============================================
+def belanja():
+    keranjang = []
+    tampilkan_produk()
 
-# ==============================================
-# INPUT PRODUK
-# ==============================================
-while True:
-    pilih_id = int(input("Pilih Id produk: "))
-    if pilih_id not in produk:
-        print("Id tidak valid!")
-        continue
-
-    jumlah = int(input(f"Masukan Jumlah Pembelian {produk[pilih_id]}: "))
-
-    keranjang.append({
-        "produk": produk[pilih_id],
-        "harga": harga[pilih_id],
-        "jumlah": jumlah
-    })
-
-    lagi = input("Tambah produk lain? (Y/N): ")
-    if lagi.lower() != "y":
-        break
-
-# ==============================================
-# INPUT DATA PENERIMA
-# ==============================================
-nama = input("Nama Penerima : ")
-alamat = input("Alamat Penerima : ")
-tlp = input("No Hp: ")
-
-total_barang = 0
-
-# ==============================================
-# TAMPILKAN DETAIL PRODUK
-# ==============================================
-print("="*70)
-print("No | Nama Barang        | Harga Satuan   | Jumlah | Total")
-print("-"*70)
-
-no = 1
-for item in keranjang:
-    subtotal = item["harga"] * item["jumlah"]
-    total_barang += subtotal
-
-    print(f"{no:<3}| {item['produk']:18} | Rp {item['harga']:12,} | {item['jumlah']:6} | Rp {subtotal:,}")
-    no += 1
-
-print("-"*70)
-
-# ==============================================
-# DISKON
-# ==============================================
-if total_barang >= 20000000:
-    diskon = int(total_barang * 0.10)
-    persen = "10%"
-elif total_barang >= 10000000:
-    diskon = int(total_barang * 0.05)
-    persen = "5%"
-else:
-    diskon = 0
-    persen = "0%"
-
-# ==============================================
-# METODE PENGIRIMAN
-# ==============================================
-print("\n================== METODE PENGIRIMAN ==================")
-for i in kurir:
-    print(f"{i:<3}| {kurir[i]}")
-print("-"*60)
-
-pilih_kirim = int(input("Pilih metode pengantaran: "))
-
-if pilih_kirim == 1:
-    metode_kirim = "Ambil di Toko"
-    ongkir = 0
-    estimasi = "Langsung Bisa Diambil"
-elif pilih_kirim == 2:
-    metode_kirim = "Kurir Instan"
-    ongkir = 70000
-    estimasi = "Dikirim Langsung"
-elif pilih_kirim == 3:
-    metode_kirim = "Kurir Reguler"
-    ongkir = 40000
-    estimasi = "Tiba ± 5 Jam"
-else:
-    metode_kirim = "Ambil di Toko"
-    ongkir = 0
-    estimasi = "Langsung Bisa Diambil"
-
-total_bayar = total_barang - diskon
-total_akhir = total_bayar + ongkir
-
-print("-"*60)
-print(f"Jumlah Bayar  : Rp {total_barang:,.0f}")
-print(f"Diskon {persen:<4}   : Rp {diskon:,.0f}")
-print(f"Total Bayar   : Rp {total_bayar:,.0f}")
-print(f"Ongkir        : Rp {ongkir:,.0f}")
-print(f"Total Akhir   : Rp {total_akhir:,.0f}")
-print("-"*60)
-
-# ==============================================
-# PEMBAYARAN
-# ==============================================
-tgl = date.today()
-
-for i in range(1, 11):
-    print(f"Loading: {i*10}%", end="\r")
-    time.sleep(0.1)
-
-konfir = input("\nApakah anda yakin ingin melakukan pembayaran? (Y/N): ")
-
-if konfir.lower() == "y":
     while True:
-        bayar = int(input("Masukan Jumlah Bayar : "))
+        pilih_id = int(input("Pilih Id Produk: "))
+        if pilih_id not in produk:
+            print("Id tidak valid!")
+            continue
+
+        jumlah = int(input(f"Jumlah {produk[pilih_id]}: "))
+
+        keranjang.append({
+            "produk": produk[pilih_id],
+            "harga": harga[pilih_id],
+            "jumlah": jumlah
+        })
+
+        lagi = input("Tambah produk lain? (Y/N): ")
+        if lagi.lower() != "y":
+            break
+
+    # DATA PENERIMA
+    nama = input("Nama Penerima : ")
+    alamat = input("Alamat        : ")
+    tlp = input("No HP         : ")
+
+    # HITUNG TOTAL
+    total_barang = sum(item["harga"] * item["jumlah"] for item in keranjang)
+
+    # DISKON
+    if total_barang >= 20000000:
+        diskon = total_barang * 0.10
+    elif total_barang >= 10000000:
+        diskon = total_barang * 0.05
+    else:
+        diskon = 0
+
+    # METODE PENGIRIMAN
+    print("\n=== METODE PENGIRIMAN ===")
+    for i in kurir:
+        print(f"{i}. {kurir[i]}")
+
+    pilih_kirim = int(input("Pilih: "))
+
+    if pilih_kirim == 1:
+        metode_kirim = "Ambil di Toko"
+        ongkir = 0
+        estimasi = "Langsung Bisa Diambil"
+    elif pilih_kirim == 2:
+        metode_kirim = "Kurir Instan"
+        ongkir = 70000
+        estimasi = "Dikirim Langsung"
+    else:
+        metode_kirim = "Kurir Reguler"
+        ongkir = 40000
+        estimasi = "Tiba ± 5 Jam"
+
+    total_bayar = total_barang - diskon
+    total_akhir = total_bayar + ongkir
+
+    # PEMBAYARAN
+    print("\n=== PEMBAYARAN ===")
+    print(f"Total Harga : Rp {total_barang:,}")
+    print(f"Diskon      : Rp {diskon:,}")
+    print(f"Ongkir      : Rp {ongkir:,}")
+    print(f"Total Bayar : Rp {total_akhir:,}")
+
+    while True:
+        bayar = int(input("Masukan Jumlah Bayar: "))
 
         if bayar >= total_akhir:
             kembalian = bayar - total_akhir
             print("\nPembayaran Berhasil!")
             break
         else:
-            print("Uang Anda Kurang!")
-else:
-    print("\nPembayaran dibatalkan")
-    exit()
+            print("Uang kurang!")
 
-print(Back.WHITE + "")
-print(Fore.YELLOW + "\n===================== ELEKTRONIK KITA ======================")
-print(tgl)
-print(f"Nama Pengguna : {current_user}")
-print("Nama Penerima :", nama)
-print("Alamat        :", alamat)
-print("No HP         :", tlp)
-print("Pengantaran   :", metode_kirim)
-print("Estimasi      :", estimasi)
-print("-"*60)
+    # CETAK STRUK
+    cetak_struk(keranjang, nama, alamat, tlp, metode_kirim, estimasi, diskon, total_bayar, ongkir, total_akhir, bayar, kembalian)
 
-no = 1
-for item in keranjang:
-    print(f"{no}. {item['produk']} x{item['jumlah']}  = Rp {(item['harga'] * item['jumlah']):,}")
-    no += 1
+# ============================================
+# CETAK STRUK + SIMPAN RIWAYAT
+# ============================================
+def cetak_struk(keranjang, nama, alamat, tlp, metode_kirim, estimasi, diskon, total_bayar, ongkir, total_akhir, bayar, kembalian):
 
-print("-"*60)
-print(f"Diskon       : Rp {diskon:,.0f}")
-print(f"Total Bayar  : Rp {total_bayar:,.0f}")
-print(f"Ongkir       : Rp {ongkir:,.0f}")
-print(f"Total Akhir  : Rp {total_akhir:,.0f}")
-print(f"Uang Anda    : Rp.{bayar:,.0f}")
-print(f"Kembalian    : Rp.{kembalian:,.0f}")
-print("\n                       TERIMA KASIH")
-print(Style.RESET_ALL)
+    tgl = date.today()
 
-# ==============================================
-# CETAK STRUK KE FILE
-# ==============================================
-with open("riwayat.txt", "w", encoding="utf-8") as s:
-    s.write("===================== ELEKTRONIK KITA ======================\n")
-    s.write(f"{tgl}\n")
-    s.write(f"Nama Pengguna : {current_user}\n")
-    s.write(f"Nama Penerima : {nama}\n")
-    s.write(f"Alamat        : {alamat}\n")
-    s.write(f"No HP         : {tlp}\n")
-    s.write(f"Pengantaran   : {metode_kirim}\n")
-    s.write(f"Estimasi      : {estimasi}\n")
-    s.write("------------------------------------------------------------\n")
+    print("\nStruk berhasil dicetak ke riwayat.txt")
 
-    no = 1
-    for item in keranjang:
-        subtotal = item['harga'] * item['jumlah']
-        s.write(f"{no}. {item['produk']} x{item['jumlah']} = Rp {subtotal:,}\n")
-        no += 1
+    with open(riwayat_file, "a", encoding="utf-8") as s:
 
-    s.write("------------------------------------------------------------\n")
-    s.write(f"Diskon       : Rp {diskon:,}\n")
-    s.write(f"Total Bayar  : Rp {total_bayar:,}\n")
-    s.write(f"Ongkir       : Rp {ongkir:,}\n")
-    s.write(f"Total Akhir  : Rp {total_akhir:,}\n")
-    s.write(f"Uang Anda    : Rp {bayar:,}\n")
-    s.write(f"Kembalian    : Rp {kembalian:,}\n")
-    s.write("\n                     TERIMA KASIH\n")
+        s.write("===================== ELEKTRONIK KITA ======================\n")
+        s.write(f"{tgl}\n")
+        s.write(f"Nama Pengguna : {current_user}\n")
+        s.write(f"Nama Penerima : {nama}\n")
+        s.write(f"Alamat        : {alamat}\n")
+        s.write(f"No HP         : {tlp}\n")
+        s.write(f"Pengantaran   : {metode_kirim}\n")
+        s.write(f"Estimasi      : {estimasi}\n")
+        s.write("------------------------------------------------------------\n")
 
-print("\nStruk berhasil dicetak ke file: riwayat.txt")
+        for i, item in enumerate(keranjang, start=1):
+            subtotal = item['harga'] * item['jumlah']
+            s.write(f"{i}. {item['produk']} x{item['jumlah']} = Rp {subtotal:,}\n")
+
+        s.write("------------------------------------------------------------\n")
+        s.write(f"Diskon       : Rp {diskon:,}\n")
+        s.write(f"Total Bayar  : Rp {total_bayar:,}\n")
+        s.write(f"Ongkir       : Rp {ongkir:,}\n")
+        s.write(f"Total Akhir  : Rp {total_akhir:,}\n")
+        s.write(f"Uang Anda    : Rp {bayar:,}\n")
+        s.write(f"Kembalian    : Rp {kembalian:,}\n")
+        s.write("============================================================\n\n")
+
+# ============================================
+# MENU UTAMA
+# ============================================
+def menu_utama():
+    while True:
+        print("\n===== MENU UTAMA =====")
+        print("1. Belanja")
+        print("2. Lihat Produk")
+        print("3. Logout")
+        print("4. Keluar")
+
+        pilihan = input("Pilih menu: ")
+
+        if pilihan == "1":
+            belanja()
+
+        elif pilihan == "2":
+            tampilkan_produk()
+
+        elif pilihan == "3":
+            print("Logout berhasil!\n")
+            return  # kembali ke menu login
+
+        elif pilihan == "4":
+            print("Terima kasih telah menggunakan aplikasi!")
+            exit()
+        else:
+            print("Pilihan tidak valid!")
+
+# ============================================
+# PROGRAM UTAMA
+# ============================================
+while True:
+    print("\n====== SELAMAT DATANG ======")
+    print("1. Login")
+    print("2. Register")
+    print("3. Keluar")
+
+    pilih = input("Pilih menu: ")
+
+    if pilih == "1":
+        if login():
+            menu_utama()
+
+    elif pilih == "2":
+        register()
+
+    elif pilih == "3":
+        print("Terima kasih!")
+        break
+
+    else:
+        print("Pilihan tidak valid!")
